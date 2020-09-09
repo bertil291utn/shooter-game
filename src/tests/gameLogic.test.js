737ec09ee9bd0aad3834fddb388358281cd60e96
Bertil Tandayamo <bertilt4@gmail.com>
2020-09-09 16:13:51 -0500

    Refacor linter sissues

--- src/tests/gameLogic.test.js
+++ src/tests/gameLogic.test.js
@@ -2,11 +2,11 @@ import 'jest-canvas-mock';
 import UserScene from '../scenes/UserScene';
 
 describe('When user input username', () => {
-  const userScene=new UserScene
+  const userScene = new UserScene();
   it('returns true if there are more than 4 characters', () => {
     expect(userScene.moreThanFourChars('betos')).toBeTruthy()
   });
   it('returns false if there are less than 4 characters', () => {
-    expect(userScene.moreThanFourChars('bes')).toBeFalsy()
-  })
-})
+    expect(userScene.moreThanFourChars('bes')).toBeFalsy();
+  });
+});
