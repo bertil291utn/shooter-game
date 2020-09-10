1f2e6aef71d8e92f7c95ccc94d7b553592704617
Bertil Tandayamo <bertilt4@gmail.com>
2020-09-10 10:55:44 -0500

    Test for set player in local storage

--- src/tests/gameLogic.test.js
+++ src/tests/gameLogic.test.js
@@ -3,7 +3,9 @@ import 'jest-canvas-mock';
 import UserScene from '../scenes/UserScene';
 import MainScene from '../scenes/MainScene';
 import PlayerModel from '../PlayerModel';
+import localStorageMethod from '../localStorage';
 
+const key = 'gameShooter';
 describe('When user input username', () => {
   const userScene = new UserScene();
   it('returns true if there are more than 4 characters', () => {
@@ -16,7 +18,6 @@ describe('When user input username', () => {
 
 describe('Main scene', () => {
   const mainScene = new MainScene();
-  const key = 'gameShooter';
   localStorage.setItem(key, JSON.stringify({ _username: 'bertil', _score: 0 }));
   it('returns User if it\'s not already set localStorage item', () => {
     expect(mainScene.currentScene('shooterGame')).toBe('User');
@@ -24,5 +25,15 @@ describe('Main scene', () => {
 
   it('return true if currentSceneVar is game', () => {
     expect(mainScene.assignPlayerObject('Game', localStorage.getItem(key))).toBeInstanceOf(PlayerModel);
+  });
+});
+
+describe('Local storage', () => {
+  const playerModel = new PlayerModel();
+  playerModel.username = 'betos';
+  playerModel.score = 0;
+  it('returns true if the local storage was save', () => {
+    localStorageMethod.setPlayer(key, playerModel);
+    expect(localStorage.getItem(key)).not.toBeNull();
   });
 });
