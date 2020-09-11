5fa0c36ceb64c329f8525cbc26ddda7090a54667
Bertil Tandayamo <bertilt4@gmail.com>
2020-09-10 21:33:01 -0500

    Remove dist from gitignore

--- src/localStorage.js
+++
@@ -1,9 +0,0 @@
-const localStorageMethod = (() => {
-  const setPlayer = (key, player) => {
-    localStorage.setItem(key, JSON.stringify(player));
-  };
-
-  return { setPlayer };
-})();
-
-export default localStorageMethod;
